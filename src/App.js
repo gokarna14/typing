62b5a3d0ee62c8556adcbeb93f1bc260a0bb85eb
prabas <prabasruck@gmail.com>
2021-06-24 15:57:31 +0545

    Final First

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Everything from './js/Everything'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, This Is Typing Tester Online</h1>
+
+    <Everything></Everything>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/js/Everything.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TypeArea from './TypeArea'
+
+
+class Everything extends React.Component{
+
+    render() {
+        return (
+            <div>
+                <TypeArea></TypeArea>
+            </div>
+        )
+    }
+}
+
+export default Everything;
